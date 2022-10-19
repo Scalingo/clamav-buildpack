a61ebb1c85df00d0c94922b5153ba276d21f2edf
François KUBLER <francois+github@kubler.org>
2022-10-19 15:51:16 +0200

    fix(freshclam): randomize freshclam daemon start
    Makes sure all containers started together don't ask for a database
    update at the same time.
    This avoids hammering the database server and get banned.
    
    Fixes CB-SEC-002

--- profile.d/freshclam.sh
+++ profile.d/freshclam.sh
@@ -4,7 +4,14 @@
 
 if [ -z "${FRESHCLAM_DISABLE_DAEMON}" ]
 then
-    freshclam --daemon --stdout --config-file="${HOME}/clamav/freshclam.conf"
+    # Wait a random amount of time to make sure instances DO NOT start
+    # freshclam at the same time.
+    # This avoids hammering the database server. It also avoids to be banned.
+    sleep $[ ( $RANDOM % 3600 ) + 1 ]s \
+        && freshclam \
+            --config-file="${HOME}/clamav/freshclam.conf" \
+            --daemon \
+            --stdout
 fi
 
 exit 0
