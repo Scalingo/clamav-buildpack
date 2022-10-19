85adcd88420b687ce8b086358967e684d647d676
François KUBLER <francois+github@kubler.org>
2022-10-20 01:03:12 +0200

    feat(profile.d): add scripts to start clamd and freshclam
    Those scripts also restart the daemons if something goes wrong.

--- profile.d/freshclam.sh
+++ profile.d/freshclam.sh
@@ -2,8 +2,7 @@
 
 # Starts freshclam daemon unless FRESHCLAM_DISABLE_DAEMON is set.
 
-if [ -z "${FRESHCLAM_DISABLE_DAEMON}" ]
-then
+start_freshclam() {
     # Wait a random amount of time to make sure instances DO NOT start
     # freshclam at the same time.
     # This avoids hammering the database server. It also avoids to be banned.
@@ -12,6 +11,21 @@ then
             --config-file="${HOME}/clamav/freshclam.conf" \
             --daemon \
             --stdout
+}
+
+if [ -z "${FRESHCLAM_DISABLE_DAEMON}" ]
+then
+    start_freshclam
+
+    while true
+    do
+        pidof "freshclam" >/dev/null \
+            && sleep 15 \
+            || {
+                echo "Freshclam does not seem to be running. Respawning." >&2
+                start_freshclam
+            }
+    done &
 fi
 
 exit 0
