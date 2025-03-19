844c92fe12a09bf17ee65f86bf6bb70d2e4ba11c
François KUBLER <francois+github@kubler.org>
2025-03-19 18:17:41 +0100

    fix: add support for CLAMAV_PROCESS_TYPES

--- files/profile.d/010-clamav.sh
+++ files/profile.d/010-clamav.sh
@@ -5,3 +5,22 @@ export PATH
 
 LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${HOME}/clamav/lib"
 export LD_LIBRARY_PATH
+
+# Whether we should start ClamAV or not:
+CLAMAV_START=1
+
+# Get the process type name we're running in:
+current_process_type="$( echo "${CONTAINER}" | cut -d'-' -f1 )"
+
+# Create the process_types array from CLAMAV_PROCESS_TYPES:
+IFS=', ' read -r -a process_types <<< "${CLAMAV_PROCESS_TYPES:-"web"}"
+
+# Check if we are in a process type for which we want to start ClamAV:
+for p in "${process_types[@]}"; do
+	if [ "${p}" == "${current_process_type}" ]; then
+		CLAMAV_START=0
+		break
+	fi
+done
+
+export CLAMAV_START
